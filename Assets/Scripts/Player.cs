40e313ffdb02014ec6d810ecf8b0ac24d1a61a2e
Jovan Djordjevic <joca100799@gmail.com>
2024-05-09 16:29:15 +0200

    Napravljena skripta za pomeranje
    Ispravljene animacije,
    Dodate kontrole
    Dodate var za brzinu char
    Dodata fixna kaemra
    Dodat health bar temp

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] TextMeshProUGUI healthText;
+    [SerializeField] float moveSpeed = 6;
+
+    Animator anim;
+    Rigidbody2D rb;
+
+    int maxHealth = 100;
+    int currentHealth;
+
+    bool dead = false;
+
+    float moveHorizontal, moveVertical;
+    Vector2 movement;
+
+    int facingDirection = 1;
+
+    private void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    private void Update() {
+        if(dead){
+            movement = Vector2.zero;
+            anim.SetFloat("Velocity", 0);
+            return;
+        }
+
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(moveHorizontal, moveVertical).normalized;
+
+        anim.SetFloat("velocity", movement.magnitude);
+
+        if(movement.x != 0)
+            facingDirection = movement.x > 0 ? 1 : -1;
+
+        transform.localScale = new Vector2(facingDirection, 1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void FixedUpdate() {
+        rb.velocity = movement * moveSpeed;
     }
 }
