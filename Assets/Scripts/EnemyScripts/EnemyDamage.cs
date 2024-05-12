c7d6ad2239e90443d4c0f88c6cb2aa0a23f9081f
jobe216 <andrija.gojkovic216@gmail.com>
2024-05-12 21:41:26 +0200

    221

---
+++ Assets/Scripts/EnemyScripts/EnemyDamage.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    public int damageAmount = 10;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        CharacterDataClass.CharacterData.CharacterTakeDamage(damageAmount);
+        Debug.Log(CharacterDataClass.CharacterData.GetCharacterHealth());
+    }
+}
